1df1a0818f2804b8aa549bc9d7e5e8f0b5455907
claradonato <mclaralimadonato@gmail.com>
2025-08-10 00:50:18 -0300

    montando primeira aplicação

---
+++ 1-crie-primeira-aplicacao/app.py
@@ -0,0 +1,45 @@
+import os
+
+def exibir_nome_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair')
+
+def finalizar_app():
+    os.system('clear'); # 'cls' no windows
+    print('Finalizando o app...\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção {opcao_escolhida}')
+
+    match opcao_escolhida:
+        case 1:
+            print('Adicionar restaurantes')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            print('Finalizar app')
+        case _: # padrão curinga
+            print('Opção inválida')
+
+# simples para a manutenção
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
