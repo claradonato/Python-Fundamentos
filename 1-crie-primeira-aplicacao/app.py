5fa9629ed0d1c8f80f8985b08fc22bc744d826b3
claradonato <mclaralimadonato@gmail.com>
2025-08-12 22:22:47 -0300

    listas e dicionários

--- 1-crie-primeira-aplicacao/app.py
+++ 1-crie-primeira-aplicacao/app.py
@@ -1,5 +1,8 @@
 import os
 
+restaurantes = [{'nome':'Pizza Hut', 'categoria': 'Fast food', 'ativo': False}, 
+                {'nome': 'VeggieFood', 'categoria': 'vegetariano', 'ativo': True}]
+
 def exibir_nome_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -12,31 +15,89 @@ def exibir_nome_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar restaurante')
     print('4. Sair')
 
 def finalizar_app():
-    os.system('clear'); # 'cls' no windows
-    print('Finalizando o app...\n')
+    exibir_subtitulo('Finalizando app...')
+
+def voltar_ao_menu():
+    input('Digite uma tecla para voltar para o menu: ')
+    main()
+
+def opcao_invalida():
+    print('Opcao invalida')
+    voltar_ao_menu()
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    linha = '*' * (len(texto) + 4)
+    print(linha)
+    print(texto)
+    print(linha)
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+
+    nome_restaurante = input('Digite o nome do restaurante: ')
+    categoria = input(f'Digite a categoria do {nome_restaurante}: ')
+
+    dados_restaurante = {'nome': nome_restaurante, 'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_restaurante)
+    print(f'Restaurante {nome_restaurante} cadastrado com sucesso!')
+    voltar_ao_menu()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listar restaurantes')
+
+    print(f'{'Nome do restaurante'.ljust(23)} | {'Categoria'.ljust(20)} | Status')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f' - {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+    
+    voltar_ao_menu()
+
+def alternar_estado():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {nome} foi desativado com sucesso!'
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+
+    voltar_ao_menu()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
 
-    match opcao_escolhida:
-        case 1:
-            print('Adicionar restaurantes')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            print('Finalizar app')
-        case _: # padrão curinga
-            print('Opção inválida')
-
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            alternar_estado()
+        elif opcao_escolhida == 4:
+            print('Sair do sistema')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    
+    except:
+        opcao_invalida()
+    
 # simples para a manutenção
 def main():
+    os.system('clear')
     exibir_nome_programa()
     exibir_opcoes()
     escolher_opcao()
---
+++ listas.py
@@ -0,0 +1,81 @@
+# Exercícios
+
+# Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+livros_kafka = ['A Metamorfose', 'O Processo']
+for livro in livros_kafka:
+    print(livro)
+
+# Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+soma = 0
+for num in range(1, 11):
+    if num%2 != 0:
+        soma += num
+print(soma)
+
+# Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente. 
+for num in range(10, 0, -1):
+    print(num)
+
+# Solicite ao usuário um número e, em seguida, utilize um loop for para 
+# imprimir a tabuada desse número, indo de 1 a 10.
+num = int(input('Digite um numero: '));
+for tab in range(1, 11):
+    print(f'{num} x {tab} = {num*tab}')
+
+#Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. 
+# Utilize um bloco try-except para lidar com possíveis exceções.
+soma_lista = 0
+numeros = [1, 4, 2, 5, 6]
+try:
+    for elem in numeros:
+        soma_lista += elem
+    print(soma_lista)
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
+
+
+# Construa um código que calcule a média dos valores em uma lista. 
+# Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
+valores = [8.0, 9.0, 10, 8.7]
+media = 0
+
+try:
+    for valor in valores:
+        media += valor
+    media = media / len(valores)
+    print(f'Média dos valores: {media}')
+except ZeroDivisionError:
+    print('A lista está vazia, não é possível calcular a média')
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
+
+# Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+pessoa = {'nome': 'Franz Kafka', 'idade': 25, 'cidade': 'Praga'}
+
+#Utilizando o dicionário criado no item 1:
+#Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+#Adicione um campo de profissão para essa pessoa; Remova um item do dicionário.
+
+pessoa['idade'] = 26
+pessoa['profissao'] = 'Escritor'
+del pessoa['cidade']
+
+#Crie um dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.
+numeros_quadrados = {x: x**2 for x in range(1, 6)}
+print(numeros_quadrados)
+
+#Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário. 
+filosofos = [{'nome': 'Platão', 'obra': 'Os últimos dias de Sócrates'}, {'nome': 'Dostoiévski', 'obra': 'Noites brancas'}]
+
+if 'obra' in filosofos:
+    print('A chave 'obra' existe no dicionário')
+else:
+    print('A chave não existe no dicionário')
+
+# Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+frase_kafka = 'Mesmo no caso de a esperança ser muito pequena, não tenho o direito de não usar as minhas possibilidades'
+contar_palavras = {}
+palavras = frase_kafka.split()
+for palavra in palavras:
+    contar_palavras[palavra] = contar_palavras.get(palavra, 0) + 1
+print(contar_palavras)
