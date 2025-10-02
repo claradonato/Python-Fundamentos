8c17ac612335c1e1e46df41a0c87ab0df4a55c55
claradonato <mclaralimadonato@gmail.com>
2025-10-02 09:29:04 -0300

    fast api cardápios

---
+++ OO-Restaurante/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Query #fastapi = modulo e FastAPI = classe
+import requests
+
+app = FastAPI()
+
+@app.get("/api/hello")
+def hello_world():
+    return {'Hello':'World'}
+
+@app.get("/api/restaurantes/")
+def get_restaurantes(restaurante: str = Query(None)):
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url) #solicitar os dados
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados':dados_json}
+        
+        #criar um dicionário
+        dados_restaurante = []
+        for item in dados_json: #separando os cardápios de cada restaurante
+            if item['Company'] == restaurante:
+                dados_restaurante[restaurante].append({
+                    'item': item['Item'],
+                    'price': item['price'],
+                    'description': item['description']
+
+                })
+        
+        return {'Restaurante': restaurante, 'Cardápio': dados_restaurante}
+
+    else:
+        return {'Erro': f'{response.status_code} - {response.text}'}
